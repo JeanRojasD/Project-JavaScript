b6889b0a86c55d96272b3fb12401aae6477ca70d
Jean Rojas <jeanrojas2007@outlook.com>
2023-03-24 06:14:25 -0300

    estatic table

--- src/pages/Orders/orders.tsx
+++ src/pages/Orders/orders.tsx
@@ -1,13 +1,34 @@
 import Menu from "../../components/Menu/menu"
 
-const Orders = () =>{
-    return(
-        <div>
-            <Menu />
-            <p>Pedidos</p>
-        </div>
-    )
-}
+import remove from '../../assets/images/remove.png'
+import edit from '../../assets/images/edit.png'
 
+import './orders.scss'
+
+const Orders = () => {
+  return (
+    <div>
+      <Menu />
+      <div className="table-content">
+        <table>
+          <tr>
+            <th>Pedido</th>
+            <th>Usuario</th>
+            <th>Status</th>
+            <th>Editar</th>
+            <th>Remover</th>
+          </tr>
+          <tr>
+            <td>Batmovel</td>
+            <td>Bruce Wayne</td>
+            <td>Pendente</td>
+            <td><img src={edit} className="icons" alt="edit"></img></td>
+            <td><img src={remove} className="icons" alt="remove"></img></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Orders;
--- src/pages/Users/users.tsx
+++ src/pages/Users/users.tsx
@@ -1,13 +1,34 @@
 import Menu from "../../components/Menu/menu"
 
-const Users = () =>{
-    return(
-        <div>
-            <Menu />
-            <p>Usuarios</p>
-        </div>
-    )
-}
+import remove from '../../assets/images/remove.png'
+import edit from '../../assets/images/edit.png'
 
+import './users.scss'
+
+const Users = () => {
+  return (
+    <div>
+      <Menu />
+      <div className="table-content">
+        <table>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Status</th>
+                <th>Editar</th>
+                <th>Remover</th>
+            </tr>
+            <tr>
+                <td>Bruce Wayne</td>
+                <td>bruce.wayne@wayne.com</td>
+                <td>Ativo</td>
+                <td><img src={edit} className="icons" alt="edit"></img></td>
+                <td><img src={remove} className="icons" alt="remove"></img></td>
+            </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Users;
